6229384ce1b4e714e6c7f5a452a1ded6ea3441c0
Nicolas Marzullo <marzulloncolas@gmail.com>
2018-10-24 17:03:15 -0300

    Contempla un par de test.
    habría que conocer la forma de optimizar el cógido pero funca.

--- src/edu/unlam/hora/Hora.java
+++ src/edu/unlam/hora/Hora.java
@@ -1,24 +1,130 @@
 package edu.unlam.hora;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hora {
-	
+
 	public static String FormatoAmigable(int seg) {
-		int años;
-		int dias;
-		int horas;
-		int minutos;
-		int segundos;
-		
-		if(seg < 0)
+		int años, restoAños, dias, restoDias, horas, restoHoras, minutos, segundos, cantComponentes =0, ultComponente = -1;
+		String retorno = "";
+		int[] componentes = new int[5];
+
+		if (seg < 0)
 			return "Error";
-		if(seg == 0)
+		if (seg == 0)
 			return "Ahora";
+
+		años = seg / 31536000;
+		restoAños = seg % 31536000; // segundos que tiene 1 año
+		dias = restoAños / 86400; // segundos que tiene un 1 dia
+		restoDias = restoAños % 86400;
+		horas = restoDias / 3600;
+		restoHoras = restoDias % 3600;
+		minutos = restoHoras / 60;
+		segundos = restoHoras % 60;
+
+		// agrego un 1 si va en singular, un 2 si va va en plural.
+		// La posición 0 corresponde a los años, la 1 a los días y asi sucesivamente
+		if (años == 1)
+			componentes[0] = 1;
+		if (años > 1)
+			componentes[0] = 2;
+		if (dias == 1)
+			componentes[1] = 1;
+		if (dias > 1)
+			componentes[1] = 2;
+		if (horas == 1)
+			componentes[2] = 1;
+		if (horas > 1)
+			componentes[2] = 2;
+		if (minutos == 1)
+			componentes[3] = 1;
+		if (minutos > 1)
+			componentes[3] = 2;
+		if (segundos == 1)
+			componentes[4] = 1;
+		if (segundos > 1)
+			componentes[4] = 2;
+
+		/* Verifico cual es el ultimo componente, pues necesito saber donde poner el
+		 * "y". Aparte cuento cuantos componentes hay.
+		 */
 		
-		segundos = seg % 60;
-		minutos = seg /= 60;
+		for (int i = 0; i < componentes.length; i++) {
+			if (componentes[i] != 0) {
+				ultComponente = i;
+				cantComponentes++;
+			}
+		}
+
 		
-		
-		return "ff";
+		for (int i = 0; i < componentes.length; i++) {
+			if (componentes[i] != 0) {
+
+				if (i == 0) { //años
+					if(componentes[i] ==1)
+						retorno += años + " año";
+					else
+						retorno += años + " años";
+				}
+				else if (i == 1) { //dias
+
+					if(!retorno.equals(""))
+						if(ultComponente == 1)
+							retorno += " y ";
+						else
+							retorno += ", ";
+						
+					if(componentes[i]==1)
+						retorno += dias + " día";
+					else
+						retorno += dias + " días";
+				}
+				else if (i == 2) { //horas
+					
+					if(!retorno.equals(""))
+						if(ultComponente == 2)
+							retorno += " y ";
+						else
+							retorno += ", ";
+					
+					if(componentes[i]==1)
+						retorno += horas + " hora";
+					else
+						retorno += horas + " horas";
+				}
+
+				else if (i == 3) { //minutos
+					
+					if(!retorno.equals(""))
+						if(ultComponente == 3)
+							retorno += " y ";
+						else
+							retorno += ", ";
+						
+					if(componentes[i]==1)
+						retorno += minutos + " minuto";
+					else
+						retorno += minutos + " minutos";
+
+				} else { //segundos
+					
+					if(!retorno.equals(""))
+						if(ultComponente == 4)
+							retorno += " y ";
+						else
+							retorno += ", ";
+						
+					if(componentes[i]==1)
+						retorno += segundos + " segundo";
+					else
+						retorno += segundos + " segundos";
+				}
+			}
+			
+		}
+		return retorno;
 	}
 
 }
--- tests/edu/unlam/tests/pruebasHoraAmigable.java
+++ tests/edu/unlam/tests/pruebasHoraAmigable.java
@@ -5,14 +5,53 @@ import org.junit.Test;
 
 import edu.unlam.hora.Hora;
 
-
 public class pruebasHoraAmigable {
 
 	@Test
-	public void testEnunciado(){
-	
+	public void testEnunciado() {
 		System.out.println(Hora.FormatoAmigable(62));
 		Assert.assertEquals("1 minuto y 2 segundos", Hora.FormatoAmigable(62));
-		
+	}
+
+	@Test
+	public void testEnunciado2() {
+		System.out.println(Hora.FormatoAmigable(3662));
+		Assert.assertEquals("1 hora, 1 minuto y 2 segundos", Hora.FormatoAmigable(3662));
+	}
+
+	@Test
+	public void testTodosLoscomponentes0() {
+		System.out.println(Hora.FormatoAmigable(98032253));
+		Assert.assertEquals("3 años, 39 días, 15 horas, 10 minutos y 53 segundos", Hora.FormatoAmigable(98032253));
+	}
+
+	@Test
+	public void testAñoUnicamente() {
+		System.out.println(Hora.FormatoAmigable(31536000));
+		Assert.assertEquals("1 año", Hora.FormatoAmigable(31536000));
+	}
+
+	@Test
+	public void testAñosUnicamente() {
+		System.out.println(Hora.FormatoAmigable(63072000));
+		Assert.assertEquals("2 años", Hora.FormatoAmigable(63072000));
+	}
+
+	@Test
+	public void testAñoYDia() {
+		System.out.println(Hora.FormatoAmigable(31622400));
+		Assert.assertEquals("1 año y 1 día", Hora.FormatoAmigable(31622400));
+	}
+
+	@Test
+	public void testAñosYDia() {
+		System.out.println(Hora.FormatoAmigable(94694400));
+		Assert.assertEquals("3 años y 1 día", Hora.FormatoAmigable(94694400));
+	}
+
+	@Test
+	public void testAñosYDias() {
+		System.out.println(Hora.FormatoAmigable(94867200));
+		Assert.assertEquals("3 años y 3 días", Hora.FormatoAmigable(94867200));
 	}
 }
